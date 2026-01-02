4fb11d09c4e02c299d7e13b81202622909ea975e
samm329-ui <jishudas329@gmail.com>
2026-01-03 03:34:19 +0530

    Fix CORS

--- next.config.ts
+++ next.config.ts
@@ -36,10 +36,9 @@ const nextConfig: NextConfig = {
         // matching all API routes
         source: "/api/:path*",
         headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this with your actual origin in production if needed, e.g. "https://web-doc-frontend.vercel.app"
-          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization, X-Requested-With" },
         ]
       }
     ]
